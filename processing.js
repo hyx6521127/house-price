f07b3b7f74b29f20055968fddd9aa119800117da
hyx6521127 <562157456@qq.com>
2024-10-11 15:49:56 +0800

    fix: 修改cookie

--- processing.js
+++ processing.js
@@ -1,24 +1,33 @@
 import fs from 'fs'
 
-fs.readFile('./result.json','utf-8', (err,result) => {
+fs.readFile('./daxing.json', 'utf-8', (err, result) => {
     const data = JSON.parse(result)
     const newData = data.filter((item, index) => {
-        if(item.address === "龙溪源" || item.address === "凤翔北路129号" || item.address === "天麓湾")
-        return false
+        if (
+            item.address === "龙溪源" || 
+            item.address === "观锦中央学府B区" || 
+            item.address === "顺富康城" || 
+            item.address === "花间集" ||
+            item.address === "金樾澜岸二期 " || 
+            item.address === "观锦中央学府A区" || 
+            item.address === "川航雅郡" ||
+            item.address === "沙湾小区"
+        )
+            return false
         else
-        return true
+            return true
     })
 
-    const lowHouse = newData.filter(item => item.unitPrice <= 6000 && item.area >= 100 && item.totalPrice < 60)
+    const lowHouse = newData.filter(item => item.floor < 20 && item.area >= 100)
 
-    const sortData = lowHouse.sort((a,b) => {
+    const sortData = lowHouse.sort((a, b) => {
         return Number(a.unitPrice) - Number(b.unitPrice)
     })
 
     const filter = JSON.stringify(sortData)
 
     fs.writeFile('./filter.json', filter, (err) => {
-        if(err) {
+        if (err) {
             console.log(err)
             return
         }
